c4535f0e30a8f1012157f97c200cb864b5d63bc5
sannimdev <inegg.apps@gmail.com>
2021-10-31 19:51:04 +0900

    현재 활성 씬 반영하기

--- js/main.js
+++ js/main.js
@@ -53,14 +53,23 @@
     ];
 
     function setLayout() {
-        // 각 스크롤 섹션의 높이 세팅하기
+        // STEP1. 각 스크롤 섹션의 높이 설정하기
         for (let i = 0; i < sceneInfo.length; i++) {
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
 
-        console.log(sceneInfo);
+        yOffset = window.pageYOffset;
+
+        let totalScrollHeight = 0;
+        for (let i = 0; i < sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+        document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
     function scrollLoop() {
@@ -80,14 +89,18 @@
             currentScene--;
         }
 
+        document.body.setAttribute('id', `show-scene-${currentScene}`);
         console.log(yOffset, currentScene);
     }
 
-    window.addEventListener('resize', setLayout);
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
         scrollLoop();
     });
-
-    setLayout();
+    /*
+        DOMContentLoaded: DOM의 구성 작업이 완료되었을 때
+        load는 웹페이지의 리소스까지 전부 로드되었을 때 
+    */
+    window.addEventListener('load', setLayout);
+    window.addEventListener('resize', setLayout);
 })();
