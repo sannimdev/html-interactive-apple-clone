f6e4af9c6eb508c3e7a446bb37c5973a6b2c57e6
sannimdev <inegg.apps@gmail.com>
2021-11-15 22:21:51 +0900

    캔버스를 창 사이즈에 맞추기

--- js/main.js
+++ js/main.js
@@ -135,6 +135,9 @@
             }
         }
         document.body.setAttribute('id', `show-scene-${currentScene}`);
+
+        const heightRatio = window.innerHeight / 1080;
+        sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
     }
 
     function calcValues(values, currentYOffset) {
@@ -176,6 +179,7 @@
             case 0:
                 let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
                 objs.context.drawImage(objs.videoImages[sequence], 0, 0);
+                objs.canvas.style.opacity = calcValues(values.canvas_opacity, currentYOffset);
 
                 if (scrollRatio <= 0.22) {
                     // in
