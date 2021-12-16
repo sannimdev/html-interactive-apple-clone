10eda7f34718881a9df7721028d1d7932a73b865
sannimdev <inegg.apps@gmail.com>
2021-12-16 20:35:29 +0900

    브라우저 리사이즈 대응

--- js/main.js
+++ js/main.js
@@ -153,7 +153,6 @@
             sceneInfo[3].objs.images.push(imgElem3);
         }
     }
-    setCanvasImages();
 
     function checkMenu() {
         if (yOffset > 44) {
@@ -675,5 +674,12 @@
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
         sceneInfo[2].objs.context.drawImage(sceneInfo[2].objs.videoImages[0], 0, 0);
     });
-    window.addEventListener('resize', setLayout);
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 600) {
+            setLayout();
+        }
+        sceneInfo[3].values.rectStartY = 0;
+    });
+    window.addEventListener('orientationchange', setLayout);
+    setCanvasImages();
 })();
