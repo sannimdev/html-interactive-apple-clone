16e9a19ed10a2ce28dd2dce4d8dc374d60a962a5
sannimdev <inegg.apps@gmail.com>
2021-12-21 19:15:56 +0900

    초기화 시점 수정

--- js/main.js
+++ js/main.js
@@ -632,17 +632,6 @@
         // console.log(yOffset, currentScene);
     }
 
-    window.addEventListener('scroll', () => {
-        yOffset = window.pageYOffset;
-        scrollLoop();
-        checkMenu();
-
-        if (!rafState) {
-            rafId = requestAnimationFrame(loop);
-            rafState = true;
-        }
-    });
-
     function loop() {
         delayedYOffset = delayedYOffset + (yOffset - delayedYOffset) * acc;
 
@@ -674,14 +663,26 @@
         setLayout();
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
         sceneInfo[2].objs.context.drawImage(sceneInfo[2].objs.videoImages[0], 0, 0);
+
+        window.addEventListener('scroll', () => {
+            yOffset = window.pageYOffset;
+            scrollLoop();
+            checkMenu();
+
+            if (!rafState) {
+                rafId = requestAnimationFrame(loop);
+                rafState = true;
+            }
+        });
+        window.addEventListener('resize', () => {
+            if (window.innerWidth > 600) {
+                setLayout();
+            }
+            sceneInfo[3].values.rectStartY = 0;
+        });
+        window.addEventListener('orientationchange', setLayout);
     });
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 600) {
-            setLayout();
-        }
-        sceneInfo[3].values.rectStartY = 0;
-    });
-    window.addEventListener('orientationchange', setLayout);
+
     setCanvasImages();
 
     document.querySelector('.loading').addEventListener('transitionend', (e) => {
