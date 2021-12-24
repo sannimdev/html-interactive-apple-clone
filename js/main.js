27c9f6b3af30aa111fd558f08a6f73fc3706e791
sannimdev <inegg.apps@gmail.com>
2021-12-24 19:22:36 +0900

    버그 수정 2

--- js/main.js
+++ js/main.js
@@ -664,6 +664,20 @@
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
         sceneInfo[2].objs.context.drawImage(sceneInfo[2].objs.videoImages[0], 0, 0);
 
+        let tempYOffset = yOffset;
+        let tempScrollCount = 0;
+
+        if (yOffset > 0) {
+            let siId = setInterval(() => {
+                window.scrollTo(0, tempYOffset);
+                tempYOffset += 5;
+
+                if (tempScrollCount++ > 20) {
+                    clearInterval(siId);
+                }
+            }, 20);
+        }
+
         window.addEventListener('scroll', () => {
             yOffset = window.pageYOffset;
             scrollLoop();
